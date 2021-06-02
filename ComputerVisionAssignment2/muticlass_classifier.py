889d97ed53ea75fb30a49480ceb14743b720d43b
Mohaymen Sameen <42969169+MohaymenSameen@users.noreply.github.com>
2021-06-03 01:10:29 +0200

    multiclass model

--- ComputerVisionAssignment2/muticlass_classifier.py
+++ ComputerVisionAssignment2/muticlass_classifier.py
@@ -1,4 +1,5 @@
 import keras
+import os
 from keras.models import *
 from keras.layers import *
 from keras.datasets import cifar10
@@ -9,12 +10,22 @@ from keras.utils import *
 from keras.applications.vgg16 import VGG16
 
 
-# creating training and testing datset
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
-classification = ['BD-Vacutainer', 'EDTA', 'Monovette', 'Plain', 'SGS', 'Vacuette', 'Vacutest', 'Venosafe']
+train_data_dir = os.path.dirname(r"output_path\train")
+validation_data_dir = os.path.dirname(r"output_path\val")
 
-# lets print data type of our dataset
-print(type(x_train))
-print(type(y_train))
-print(type(x_test))
-print(type(y_test))
+img_width, img_height = 220, 380
+
+def define_VGGmodel():
+    model = VGG16(include_top=False, input_shape=(img_width, img_height, 3))
+    for layer in model.layers:
+        layer.trainable = False
+    flat1 = Flatten()(model.layers[-1].output)
+    class1 = Dense(128, activation='relu', kernel_initializer='he_uniform')(flat1)
+    output = Dense(8, activation='softmax')(class1)
+    model = Model(inputs=model.inputs, outputs=output)
+    opt = SGD(lr=0.001, momentum=0.9)
+    model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['cateogrical_accuracy'])
+    return model
+
+model = define_VGGmodel()
+model.summary()
